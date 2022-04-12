e387cecf64ce76ad45c7fcfb43836c8b890e1d39
unknown <anca2009@gmail.com>
2022-04-12 18:26:33 +0200

    testing staging

---
+++ build/precache-manifest.86d28b630ecbc165df8b999677b88228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c40d003cd4be4659998a",
+    "url": "/static/css/main.c44dd77e.chunk.css"
+  },
+  {
+    "revision": "c40d003cd4be4659998a",
+    "url": "/static/js/main.c40d003c.chunk.js"
+  },
+  {
+    "revision": "629ee088d0c3fc952ac4",
+    "url": "/static/js/1.629ee088.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "1979f42fb03044999fea",
+    "url": "/static/js/runtime~main.1979f42f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b59fa1cdeb67d1ef022cbc4b7de8bee8",
+    "url": "/index.html"
+  }
+];
